078a19c200738810dafeac4039e2968bedc39067
Tristan Lenzberg <112999910+tristanlgb@users.noreply.github.com>
2023-11-13 01:33:37 -0300

    Delete app directory

--- app/app.config.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationConfig } from '@angular/core';
-
-
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
-
-export const appConfig: ApplicationConfig = {
-  providers: [ provideClientHydration(), provideAnimations(), provideAnimations()]
-};
