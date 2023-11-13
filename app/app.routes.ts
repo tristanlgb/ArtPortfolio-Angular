3b76bf24fa1e005d2b7694853bb9e655e7f21303
Tristan Lenzberg <112999910+tristanlgb@users.noreply.github.com>
2023-11-13 00:07:20 -0300

    Add files via upload

---
+++ app/app.routes.ts
@@ -0,0 +1,14 @@
+// app.routes.ts
+
+import { Routes } from '@angular/router';
+import { HomeComponent } from '../components/home/home.component';
+import { AboutMeComponent } from '../components/about-me/about-me.component';
+import { MyArtComponent } from '../components/my-art/my-art.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about-me', component: AboutMeComponent },
+  { path: 'my-art', component: MyArtComponent },
+  // Add more routes as needed
+];
+
